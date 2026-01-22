35990f6c88ffbe0c382e1485346aeacc80ffb44f
Christian Kantchev <cvk@ucsb.edu>
2026-01-22 14:51:51 -0800

    implemented contains, getNodeFor, count, and sum. All tests pass successfully

--- intbst.cpp
+++ intbst.cpp
@@ -31,7 +31,7 @@ void IntBST::clear(Node *n) {
 // insert value in tree; return false if duplicate
 bool IntBST::insert(int value) {
     if (!root) {
-        root = new Node(value)
+        root = new Node(value);
         return true;
     }
 
@@ -68,7 +68,7 @@ bool IntBST::insert(int value, Node *n) {
 // print tree data pre-order
 void IntBST::printPreOrder() const {
     printPreOrder(root);
-    cout << endl;
+    // cout << endl;
 }
 
 // recursive helper for printPreOrder()
@@ -76,46 +76,75 @@ void IntBST::printPreOrder(Node *n) const {
     //preorder iteration: root->left->right
     if (!n) return;
 
-    cout << left->info << " ";
+    cout << n->info << " ";
     printPreOrder(n->left);
     printPreOrder(n->right);
 }
 
 // print tree data in-order, with helper
 void IntBST::printInOrder() const {
-    cout << "IMPLEMENT printInOrder public method"; // IMPLEMENT HERE
+    printInOrder(root);
+    // cout << endl;
 }
 void IntBST::printInOrder(Node *n) const {
-    cout << "IMPLEMENT IMPLEMENT printInOrder private helper method"; // IMPLEMENT HERE
+    //inorder iteration: left->root->right
+    if (!n) return;
+
+    printInOrder(n->left);
+    cout << n->info << " ";
+    printInOrder(n->right);
 }
 
 // prints tree data post-order, with helper
 void IntBST::printPostOrder() const {
-    cout << "IMPLEMENT printPostOrder public method"; // IMPLEMENT HERE
+    printPostOrder(root);
+    // cout << endl;
 }
 
 void IntBST::printPostOrder(Node *n) const {
-    cout << "IMPLEMENT printPostOrder private helper method";// IMPLEMENT HERE
+    //postorder iteration: left->right->root
+    if (!n) return;
+
+    printPostOrder(n->left);
+    printPostOrder(n->right);
+    cout << n->info << " ";
 }
 
 // return sum of values in tree
 int IntBST::sum() const {
-    return -1; // REPLACE THIS NON-SOLUTION
+    // if (!root) return -1;
+    return sum(root);
 }
 
 // recursive helper for sum
 int IntBST::sum(Node *n) const {
-    return -1; // REPLACE THIS NON-SOLUTION
+    //sum of nodes is given by sum of values in left subtree
+    //+ sum of value in right subtree. 
+    //postorder traversal
+
+    if (!n) return 0;
+
+    int left = sum(n->left);
+    int right = sum(n->right);
+    return n->info + left + right;
 }
 
 // return count of values
 int IntBST::count() const {
-    return -1; // REPLACE THIS NON-SOLUTION
+    // if (!root) return -1;
+    return count(root);
 }
 
 // recursive helper for count
 int IntBST::count(Node *n) const {
-    return -1; // REPLACE THIS NON-SOLUTION
+    //similar to sum. count is equal to count in left subtree + count in right subtree + 1;
+    //postorder traversal, add 1 to the running count on each step.
+    if (!n) return 0; //null nodes should count as 0;
+
+    return count(n->left) + count(n->right) + 1;
+    //since current node is processed last, this is still a postorder traversal (even though
+    // it is only on one line.)
+
 }
 
 // IMPLEMENT THIS FIRST: returns the node for a given value or NULL if none exists
@@ -125,12 +154,43 @@ int IntBST::count(Node *n) const {
 // Whenever you call this method from somewhere else, pass it
 // the root node as "n"
 IntBST::Node* IntBST::getNodeFor(int value, Node* n) const{
-    return NULL; // REPLACE THIS NON-SOLUTION
+    //Basically: search for a value in a BST
+    //binary search?
+    if (!n) return NULL; //leaf node reached but value not found
+    if (value < n->info) {
+        getNodeFor(value, n->left);
+    }
+    else if (value > n->info) {
+        getNodeFor(value, n->right);
+    }
+    else { //n->val = value (desired value found)
+        return n;
+    }
 }
 
 // returns true if value is in the tree; false if not
 bool IntBST::contains(int value) const {
-    return false; // REPLACE THIS NON-SOLUTION
+    //recursive solution would involve calling getNodeFor with root, and
+    //returning true if return is a node or false if return is null
+
+    //iteratively:
+    //binary search within the tree:
+    //since this is a binary tree, stepping into left or right is equivalent
+    //to halving the search space. Thus, we maintain a runtime of O(logn)
+    Node* current = root;
+    while (current) {
+        if (value == current->info) {
+            return true;
+        }
+        else if (value < current->info) {
+            current = current->left;
+        }
+        else { //value > current
+            current = current->right;
+        }
+    }
+
+    return false;
 }
 
 // returns the Node containing the predecessor of the given value
